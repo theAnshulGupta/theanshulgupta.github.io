50ccb7c9ffbe779089e4aa5b8c1ddcc5543529ce
Anshul Gupta <hi.anshulg@gmail.com>
2021-01-10 13:13:49 -0800

    nav css and js

--- js/main.js
+++ js/main.js
@@ -52,3 +52,47 @@ $(document).ready(function () {
     }
   });
 });
+
+// let progress = document.getElementById("scroll");
+// let p = document.getElementById("percent");
+// let max = document.body.scrollHeight - window.innerHeight;
+// let change = (window.pageYOffset / max) * 100;
+
+// window.onresize = function (event) {
+//   max = document.body.scrollHeight - window.innerHeight;
+//   change = (window.pageYOffset / max) * 100;
+// };
+
+// // console.log(change);
+// if (change == 0) {
+//   p.innerHTML = Math.round(change) + "%";
+// }
+
+// window.onscroll = function () {
+//   change = (window.pageYOffset / max) * 100;
+//   progress.style.height = change + "%";
+//   //   p.innerHTML = "Page Scrolled " + Math.round(change) + "%";
+//   p.innerHTML = Math.round(change) + "%";
+// };
+
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark",
+};
+
+const theme =
+  localStorage.getItem("theme") ||
+  ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem("theme");
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem("theme", next);
+}
+
+document.getElementById("themeButton").onclick = toggleTheme;
